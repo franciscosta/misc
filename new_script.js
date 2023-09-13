4decca29304bcec5531bee09750a00af801862a5
Francisco Costa <franciscosta@hey.com>
2023-09-13 14:55:26 -0700

    Adding sound on hover

--- new_script.js
+++ new_script.js
@@ -605,26 +605,38 @@ function soundifyCheckboxes() {
   
     checkbox.addEventListener('click', function(e) {
     
-      let parentTask = e.currentTarget.closest('.s23-v2-task');
-      if (parentTask) {
-      
-        console.log('Found the parent');
+    checkbox.addEventListener('click', function(e) {
+      handleCheckboxClick(e, sounds);
+    });
 
-        let check = parentTask.querySelector('.s23-app-checkbox');
-        let stokethrough = parentTask.querySelector('.s23-app-task-completed');
-        let text = parentTask.querySelector('.s23-task-text-wrapper');
-  
-        if (check) check.classList.toggle('check-active');
-        if (stokethrough) stokethrough.classList.toggle('strokethrough-active');
-        if (text) text.classList.toggle('text-active');
-      }
-      
+    checkbox.addEventListener('mouseover', function() {
       let sound = new Audio(sounds[Math.floor(Math.random() * sounds.length)]);
       sound.play();
+    });
       
     });
   });
+}
+
+function handleCheckboxClick(e, sounds) {
+
+  let parentTask = e.currentTarget.closest('.s23-v2-task');
+  if (parentTask) {
+      
+    console.log('Found the parent');
+
+    let check = parentTask.querySelector('.s23-app-checkbox');
+    let stokethrough = parentTask.querySelector('.s23-app-task-completed');
+    let text = parentTask.querySelector('.s23-task-text-wrapper');
   
+    if (check) check.classList.toggle('check-active');
+    if (stokethrough) stokethrough.classList.toggle('strokethrough-active');
+    if (text) text.classList.toggle('text-active');
+  }
+      
+  let sound = new Audio(sounds[Math.floor(Math.random() * sounds.length)]);
+  sound.play();
+
 }
 
 soundifyCheckboxes();
