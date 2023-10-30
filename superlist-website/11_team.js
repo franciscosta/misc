1802c930549adddcce44016488274329f63f6f57
Francisco Costa <franciscosta@hey.com>
2023-10-30 16:14:46 -0700

    Adding video logic

--- superlist-website/11_team.js
+++ superlist-website/11_team.js
@@ -1,23 +1,110 @@
-// Define your API key and Collection ID
-const API_KEY = '4d688c98a68cdab9b3c8b786e71b9334719a5eb20e8d497b64d0aeef98adbe2f';
-const COLLECTION_ID = '627d50981442f62edd183f3a';
+const team = {
+  'Caspar': {
+    'location': 'Berlin',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1676995954/website/team/Caspar_vykk29.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1676995954/website/team/Caspar_vykk29.webm'
+  },
+  'Mai': {
+    'location': 'Barcelona',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1676995953/website/team/Mai1_qngeby.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1676995953/website/team/Mai1_qngeby.webm',
+  },
+  'Brian': {
+    'location': 'Lake District',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1675855471/website/team/brian_pols7l.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1675855471/website/team/brian_pols7l.webm',
+  },
+  'Niklas': {
+    'location': 'Berlin',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1675855468/website/team/Niklas_tdduwp.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1675855468/website/team/Niklas_tdduwp.webm',
+  },
+  'Hannah': {
+    'location': 'Amsterdam',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_800/v1672862537/website/team/Hannah_xjn2ta.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_800/v1672862537/website/team/Hannah_xjn2ta.webm',
+  },
+  'Daria': {
+    'location': 'Berlin',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125372/website/team/Daria_rgu5kk.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125372/website/team/Daria_rgu5kk.webm',
+  },
+  'Julian': {
+    'location': 'Medellin',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125371/website/team/Julian_eg7zej.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125371/website/team/Julian_eg7zej.webm',
+  },
+  'Nic': {
+    'location': 'Rome',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125375/website/team/nic_xegw92.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125375/website/team/nic_xegw92.webm',
+  },
+  'Ines': {
+    'location': 'Amsterdam',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125391/website/team/Ines_aend0e.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125391/website/team/Ines_aend0e.webm',
+  },
+  'Brandon': {
+    'location': 'Seattle',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125395/website/team/Brandon_gcs63x.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125395/website/team/Brandon_gcs63x.webm',
+  },
+  'Cillian': {
+    'location': 'Galway',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125407/website/team/cillian_ll3rko.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125407/website/team/cillian_ll3rko.webm',
+  },
+  'Frank': {
+    'location': 'San Francisco',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125420/website/team/Frank_k5qqlb.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125420/website/team/Frank_k5qqlb.webm',
+  },
+  'Martin': {
+    'location': 'Marum',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125442/website/team/Martin_mvfk08.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125442/website/team/Martin_mvfk08.webm',
+  },
+  'Iiro': {
+    'location': 'Tallinn',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125447/website/team/iiro_xn04ra.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125447/website/team/iiro_xn04ra.webm',
+  },
+  'Miguel': {
+    'location': 'Portimão',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125464/website/team/miguel_mwjxth.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125464/website/team/miguel_mwjxth.webm',
+  },
+  'Michael': {
+    'location': 'Marl',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125470/website/team/Michael_auk1wa.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652125470/website/team/Michael_auk1wa.webm',
+  },
+  'Cameron': {
+    'location': 'Victoria',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652204566/website/team/Cam_ut4mm3.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/c_scale,q_auto:good,w_500/v1652204566/website/team/Cam_ut4mm3.webm',
+  },
+  'Eniola': {
+    'location': 'London',
+    'mp4': 'https://res.cloudinary.com/superlist/video/upload/q_auto:good/v1655117456/website/team/Eniola_converted_khwhad.mp4',
+    'webm': 'https://res.cloudinary.com/superlist/video/upload/q_auto:good/v1655117456/website/team/webm',
+  },
+}
 
-// Set up the API endpoint
-const endpoint = `https://api.webflow.com/collections/${COLLECTION_ID}/items`;
+const names = Object.keys(team);
 
-// Make the API request
-fetch(endpoint, {
-  method: 'GET',
-  headers: {
-    'Authorization': `Bearer ${API_KEY}`,
-    'accept-version': '1.0.0',
-    'Content-Type': 'application/json'
-  }
-})
-.then(response => response.json())
-.then(data => {
-  console.log(data.items); // This will log the items from the collection to the console
-})
-.catch(error => {
-  console.error('There was an error fetching the collection:', error);
+const cells = document.querySelectorAll('.careers23-individual-item');
+
+cells.forEach(cell => {
+
+  const currentName = names.pop();
+
+  cell.innerHTML = `
+  <video autoplay muted loop playsinline class="careers23-team-video">
+    <source src="${team[currentName].webm}" type="video/webm">
+  </video>
+  `;
+
 });
+
+
