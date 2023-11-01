08dd79e485f04b050022e289476103348abea195
Francisco Costa <franciscosta@hey.com>
2023-11-01 14:35:14 -0700

    Adding dark mode

--- superlist-website/11_changelog_darkmode.js
+++ superlist-website/11_changelog_darkmode.js
@@ -22,12 +22,6 @@ document.addEventListener("DOMContentLoaded", function() {
   );
 
   setupDarkModeImageSwitcher(
-    '.s23-logo-top',
-    'https://uploads-ssl.webflow.com/625593a881b8ebd169835ca5/650364790413cd1127e7e26b_Superlist%20logo.svg',
-    'https://uploads-ssl.webflow.com/625593a881b8ebd169835ca5/654191f437e421437154f98b_logo%20light.svg',
-  );
-
-  setupDarkModeImageSwitcher(
     '.s23-arrow-up',
     'https://uploads-ssl.webflow.com/625593a881b8ebd169835ca5/651324a35d0948418386f12b_Arrow.svg',
     'https://uploads-ssl.webflow.com/625593a881b8ebd169835ca5/654192b23fbb3726330d40b4_Arrow.svg',
